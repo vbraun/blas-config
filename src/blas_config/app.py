fa4158939b784d57974746e9368b798dac5a922a
Volker Braun <vbraun.name@gmail.com>
2014-09-28 00:30:27 +0100

    Dummy cblas.pc file

--- src/blas_config/app.py
+++ src/blas_config/app.py
@@ -3,6 +3,28 @@
 Main entry point
 """
 
+import sys
+import os
 
 
-print('test')
+CBLAS_TEMPLATE = """
+prefix=/usr
+libdir=/usr/lib64
+exec_prefix=${prefix}
+includedir=${prefix}/include
+
+Name: cblas
+Description: C Basic Linear Algebra Subprograms (CBLAS)
+Version: 1.0
+URL: http://www.gnu.org/software/gsl
+Libs: -L${libdir} -lblas
+Libs.private: -lm
+Cflags: -I${includedir}
+"""
+
+
+def write_cblas_pc(path):
+    name = 'cblas.pc'
+    os.makedirs(path)
+    with open(os.path.join(path, name), 'wt') as f:
+        f.write(CBLAS_TEMPLATE)
